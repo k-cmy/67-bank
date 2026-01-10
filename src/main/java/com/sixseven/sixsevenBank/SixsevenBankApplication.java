81a345967698365e861c396bcd6ef78cd93bf43f
Kimberley Chung Min Yi <mchu0045@student.monash.edu>
2026-01-11 00:00:57 +0800

    add external dependencies and connect project to database

---
+++ src/main/java/com/sixseven/sixsevenBank/SixsevenBankApplication.java
@@ -0,0 +1,13 @@
+package com.sixseven.sixsevenBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixsevenBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixsevenBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sixseven/sixsevenBank/SixsevenBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixseven.sixsevenBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixsevenBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
